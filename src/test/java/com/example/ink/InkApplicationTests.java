71e3e14ad6773c01a51e3cd8c2c5632888852235
dafebj <daphinefebij@gmail.com>
2025-04-07 22:04:25 +0530

    Initial commit - Maven project setup

---
+++ src/test/java/com/example/ink/InkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
